53af1721eb8710de6af0cc2bc11ef4c82365cd83
smhigley <sarahmhigley@gmail.com>
2014-04-14 17:44:03 -0500

    fixed zoom queue, updated map styles, added more data points

--- cluster.py
+++ cluster.py
@@ -4,10 +4,10 @@ import math
 
 # get data from flickr
 # build flickr url
-flickr_api_key = 'your api key here'
-flickr_api_secret = 'your secret here'
+flickr_api_key = '5f6652a08b32dd50ae264fb3f47a9632'
+flickr_api_secret = '6826da1b61cf3f39'
 tags = 'cat'
-url = 'http://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=' + flickr_api_key + '&tags=' + tags + '&woe_id=2347602&has_geo=1&extras=geo&format=json&nojsoncallback=1&per_page=100';
+url = 'http://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=' + flickr_api_key + '&tags=' + tags + '&woe_id=2347602&has_geo=1&extras=geo&format=json&nojsoncallback=1&per_page=800';
 
 # get data from url
 json_data = urllib2.urlopen(url)
@@ -20,7 +20,7 @@ min_zoom = 4
 
 # clustering settings
 clusters = {} # empty dictionary to add to later
-max_distance_base = 160.0 # max distance in km for zoom 4
+max_distance_base = 200.0 # max distance in km for zoom 4
 
 # clustering function, takes array of data and int zoom
 # returns clusters in array of [{latitude: latitude, longitude: longitude, size: # of points, points: [array of points' indices]}, ... ]
@@ -45,16 +45,16 @@ def cluster(data, zoom):
 
       if dist <= max_distance:
         # if close enough, add point to cluster, update cluster x and y, set clustered to true
-        cluster['size'] += 1
         cluster['points'].append(point['id'])
-        #cluster['longitude'] = average(x, cluster['longitude'])
-        #cluster['latitude'] = average(y, cluster['latitude'])
+        cluster['longitude'] = average(x, cluster['longitude'], cluster['size'])
+        cluster['latitude'] = average(y, cluster['latitude'], cluster['size'])
+        cluster['size'] += 1
         clustered = True
         break
 
     #if it is not close enough to join an existing cluster, create a new one
     if clustered == False:
-      new_cluster = {'longitude': x, 'latitude': y, 'size': 1, 'points': [point['id']] }
+      new_cluster = {'longitude': x, 'latitude': y, 'size': 1, 'points': [point['id']], 'zoom': zoom }
       cluster_set.append(new_cluster)
 
   return cluster_set
@@ -63,7 +63,7 @@ def cluster(data, zoom):
 def distance(origin, destination):
     lat1, lon1 = origin
     lat2, lon2 = destination
-    radius = 6371 # km
+    radius = 6371 # km radius of earth
 
     dlat = math.radians(lat2-lat1)
     dlon = math.radians(lon2-lon1)
@@ -74,9 +74,10 @@ def distance(origin, destination):
 
     return d
 
-# average of two points
-def average(n1, n2):
-  return (n1 + n2)/2
+# average of two points, weighted by size
+def average(n1, n2, size):
+  #n2 is always a cluster with [size] points, n1 size is always 1
+  return (n1 + size*n2)/(size + 1)
 
 def mode(list):
   modes = {}
